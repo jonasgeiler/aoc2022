c57e1942f81e975785043db82c6fc7051615de98
Jonas Geiler <skayo.dev@gmail.com>
2022-12-02 09:40:49 +0100

    feat: day 2

--- README.md
+++ README.md
@@ -12,7 +12,7 @@
 <!--SOLUTIONS-->
 
 [![Day](https://badgen.net/badge/01/%E2%98%85%E2%98%85/green)](src/day01)
-![Day](https://badgen.net/badge/02/%E2%98%86%E2%98%86/gray)
+[![Day](https://badgen.net/badge/02/%E2%98%85%E2%98%85/green)](src/day02)
 ![Day](https://badgen.net/badge/03/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/04/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/05/%E2%98%86%E2%98%86/gray)
@@ -69,16 +69,16 @@ npm start 1
 
 ```
 Day 01
-Time part 1: 0.878ms
-Time part 2: 0.371ms
-Both parts: 1.249271ms
+Time part 1: 0.825ms
+Time part 2: 0.56ms
+Both parts: 1.384453ms
 ```
 
 ```
 Day 02
-Time part 1: -
-Time part 2: -
-Both parts: -
+Time part 1: 2.257ms
+Time part 2: 1.881ms
+Both parts: 4.138492ms
 ```
 
 ```
@@ -243,8 +243,8 @@ Both parts: -
 ```
 
 ```
-Total stars: 2/50
-Total time: 1.249ms
+Total stars: 4/50
+Total time: 5.523ms
 ```
 
 <!--/RESULTS-->
---
+++ src/day02/index.js
@@ -0,0 +1,105 @@
+import run from "aocrunner";
+
+
+// All winning combinations
+const wins = [
+  '1 3', // Rock defeats Scissors
+  '3 2', // Scissors defeats Paper
+  '2 1', // Paper defeats Rock
+];
+
+// All possible hands
+  const hands = {
+    'A': 1, // Opponent Rock
+    'X': 1, // Player Rock
+
+    'B': 2, // Opponent Paper
+    'Y': 2, // Player Paper
+
+    'C': 3, // Opponent Scissors
+    'Z': 3, // Player Scissors
+  };
+
+
+const parseInput = (rawInput) => rawInput;
+
+const part1 = (rawInput) => {
+  const input = parseInput(rawInput);
+
+  let score = 0;
+  for (let line of input.split('\n')) {
+    const [ opponentHand, playerHand ] = line.split(' ');
+
+    score += hands[playerHand]; // Add points for player's hand
+
+    if (hands[opponentHand] === hands[playerHand]) {
+      score += 3; // Draw
+    } else if (wins.indexOf(hands[playerHand] + ' ' + hands[opponentHand]) !== -1) {
+      score += 6; // Player wins
+    }
+  }
+
+  return score;
+};
+
+const part2 = (rawInput) => {
+  const input = parseInput(rawInput);
+
+  let score = 0;
+  for (let line of rawInput.split('\n')) {
+    const [ opponentHand, outcome ] = line.split(' ');
+
+    if (outcome === 'X') { // Player needs to loose
+      const playerHandPoints = Number(wins.find(win => win.startsWith(hands[opponentHand])).split(' ')[1]); // This finds a win for the opponent, and uses the points from the loosing hand in that combination
+
+      score += playerHandPoints; // Add points for player's hand
+    } else if (outcome === 'Y') { // Player needs to make draw
+      const playerHandPoints = hands[opponentHand];
+
+      score += playerHandPoints; // Add points for player's hand
+      score += 3; // Add points for draw
+    } else if (outcome === 'Z') { // Player needs to win
+      const playerHandPoints = Number(wins.find(win => win.endsWith(hands[opponentHand])).split(' ')[0]); // This finds a win for the player, and uses the points from the winning hand in that combination
+
+      score += playerHandPoints; // Add points for player's hand
+      score += 6; // Add points for win
+    }
+  }
+
+  return score;
+};
+
+run({
+  part1: {
+    tests: [
+      // {
+      //   input: ``,
+      //   expected: "",
+      // },
+      {
+        input: `A Y
+B X
+C Z`,
+        expected: 15
+      }
+    ],
+    solution: part1,
+  },
+  part2: {
+    tests: [
+      // {
+      //   input: ``,
+      //   expected: "",
+      // },
+      {
+                input: `A Y
+B X
+C Z`,
+        expected: 12
+      }
+    ],
+    solution: part2,
+  },
+  trimTestInputs: true,
+  onlyTests: false,
+});
